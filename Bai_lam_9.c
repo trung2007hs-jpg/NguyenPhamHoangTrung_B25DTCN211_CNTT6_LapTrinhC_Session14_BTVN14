f2694ee0b6a332d396c0ab07935c3885e5ee20b3
trung2007hs-jpg <trung2007hs@gmail.com>
2025-11-11 23:59:42 +0700

    Remove GCD calculation and main function

--- Bai_lam_9.c
+++ Bai_lam_9.c
@@ -1,16 +1,2 @@
-#include <stdio.h>
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-int main() {
-    int num1 = 36, num2 = 60;
-    int result = gcd(num1, num2);
-    printf("UCLN cua %d va %d la: %d\n", num1, num2, result);
-    return 0;
-}
+
 
