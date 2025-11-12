a69ce1a2a402af25087bd905b75c80cf57dbb09f
trung2007hs-jpg <trung2007hs@gmail.com>
2025-11-12 13:24:39 +0700

    Update Bai_lam_8.c

--- Bai_lam_8.c
+++ Bai_lam_8.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include <stdbool.h>
-bool isPerfect(int n) {
+int isPerfect(int n) {
     if (n < 1)
-        return false;
+        return 0; 
     int sum = 0;
     for (int i = 1; i <= n / 2; i++) {
         if (n % i == 0)
             sum += i;
     }
-    return sum == n;
+    if (sum == n)
+        return 1; 
+    else
+        return 0;
 }
 int main() {
-    int num1, num2;
-    printf("Nhap so nguyen thu nhat: ");
-    scanf("%d", &num1);
-    printf("Nhap so nguyen thu hai: ");
-    scanf("%d", &num2);
+    int num1 = 6;
+    int num2 = 12;
+
     if (isPerfect(num1))
         printf("%d la so hoan hao\n", num1);
     else
